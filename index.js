116c173bb141569a7438f4ebe3534a8aa4c9a69c
Alark <alarksingh1999@gmail.com>
2022-03-26 12:31:24 +0530

    done

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const PostRoutes = require("./routes/PostRoutes.js");
+const PORT = 50002;
+
+const app = express();
+app.use(cors());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb' }));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+//declaring routers
+app.use("/posts", PostRoutes);
+
+//connecting to mongoDb
+const connectionUrl =
+    "mongodb+srv://alarksingh:Baba9936@mernapp.rmpfg.mongodb.net/BlogApp?retryWrites=true&w=majority";
+mongoose
+    .connect(connectionUrl, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log("Server running on port " + PORT);
+        });
+    })
+    .catch((e) => {
+        console.log("error", e.message);
+    });
